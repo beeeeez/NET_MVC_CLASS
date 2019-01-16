8f56abe88edb22dffacf9cef2d1430e083bd74e9
Brown <cibrown@labs.neit.edu>
2019-01-15 20:11:44 -0500

    first commit

---
+++ lab1_cb_adv_net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lab1_cb_adv_net.MvcApplication" Language="C#" %>
---
+++ lab1_cb_adv_net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab1_cb_adv_net.Startup))]
+namespace lab1_cb_adv_net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
